691debd15e59bc4d354fb6b9801e61e216f2a815
Rahul Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2025-10-15 23:01:47 +0530

    Update search-insert-position.java

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,7 +3,14 @@ class Solution {
         
         
         //----------------------------------- Brute force method------------------------------------------
+    
+    /* 
+    This Solution: Linear Search (O(n))
+    The first solution uses a linear search (a simple for loop) to check every element starting from the beginning.
+    In the worst-case scenario (when target is the largest number or doesn't exist and needs to be placed at the very end), the loop has to iterate through all n elements.
+    This means the time taken grows directly in proportion to the size of the input array (n). This is inefficient for large arrays.*/
 
+        
         // for(int i=0; i<nums.length;i++){
         //     if(nums[i]>=target){
         //         return i;
@@ -15,7 +22,13 @@ class Solution {
 
 
         // ----------------------------------- optimized solution-----------------------------------------
+    /*
+    This solution solution uses binary search. This algorithm works because the input array nums is sorted.
+    Binary search repeatedly divides the search interval in half.
+    In each step, it eliminates half of the remaining elements.
+    This makes the search time grow logarithmically with the size of the input. For an array of size n, the number of operations is roughly log₂(n).*/
 
+        
         int left =0;
         int right = nums.length-1;
         while(left<=right){
