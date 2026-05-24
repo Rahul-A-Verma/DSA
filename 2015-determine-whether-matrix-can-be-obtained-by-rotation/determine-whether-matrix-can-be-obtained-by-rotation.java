2edda71e7df465fc8b2913e41c51754da8da3aea
Rahul Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2026-05-24 18:10:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (74.37%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,24 +1,60 @@
 class Solution {
- public static int[][] transpose(int[][] matrix) {
-    for(int i=0;i<matrix.length;i++){
-        for(int j=i;j<matrix[i].length;j++){
-            int temp = matrix[i][j];
-            matrix[i][j]=matrix[j][i];
-            matrix[j][i]=temp;
+    // public static int[][] transpose(int[][] matrix){
+    //     for(int i=0;i<matrix.length;i++){
+    //         for(int j=i;j<matrix[i].length;j++){
+    //             int temp = matrix[i][j];
+    //             matrix[i][j]=matrix[j][i];
+    //             matrix[j][i]=temp;
+    //         }
+    //     }
+    //     return matrix;
+    // }
+    // public static int[][] reverseMat(int[][] matrix){
+    //     for(int i=0;i<matrix.length;i++){
+    //         int left=0;
+    //         int right=matrix[i].length-1;
+    //         while(left<right){
+    //             int temp=matrix[i][left];
+    //             matrix[i][left]=matrix[i][right];
+    //             matrix[i][right]=temp;
+
+    //             left++;
+    //             right--;
+    //         }
+    //     }
+    //     return matrix;
+    // }
+
+    public static int[][] rotate90(int[][]matrix){
+         for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[i].length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
         }
-    }
-    Collections.reverse(Arrays.asList(matrix));
+           for(int i=0;i<matrix.length;i++){
+            int left=0;
+            int right=matrix[i].length-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+
+                left++;
+                right--;
+            }
+        }
         return matrix;
     }
 
     public boolean findRotation(int[][] mat, int[][] target) {
-      
-       
         for(int c=0;c<4;c++){
-         boolean flag = true;
+            boolean flag=true;
+
             for(int i=0;i<mat.length;i++){
                 for(int j=0;j<mat[i].length;j++){
-                    if(mat[i][j]!= target[i][j]){
+                    if(mat[i][j]!=target[i][j]){
                         flag=false;
                         break;
                     }
@@ -28,9 +64,9 @@ class Solution {
                 }
             }
             if(flag){
-               return true; 
+                return true;
             }
-            mat = transpose(mat);
+             mat=rotate90(mat);
         }
         return false;
     }
