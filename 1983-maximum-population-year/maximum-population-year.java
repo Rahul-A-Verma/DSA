45af4fa8696be7a5066e107c4ada1755d1a99b14
Rahul Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2026-05-24 17:51:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (60.34%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.java
+++ 1983-maximum-population-year/maximum-population-year.java
@@ -1,22 +1,28 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        int[] arr = new int[101];
+    
+    int[] arr = new int[101];
+    int current=0;
+    int max=0;
+    int year=0;
+    
+    for(int[] i : logs){
+        arr[i[0]-1950]++;
+        arr[i[1]-1950]--;
+    }
 
-        for(int i=0;i<logs.length;i++){
-            arr[logs[i][0]-1950]++;
-             arr[logs[i][1]-1950]--;
+    for(int i=0;i<arr.length;i++){
+     
+        current += arr[i];
+        if(current>max){
+            max=current;
+            year=i;
         }
-        int max=0;
-        int current=0;
-        int year =0;
+        
+       
+        
+    }
+    return year+1950;
 
-        for(int i=0;i<100;i++){
-            current +=arr[i];
-            if(current>max){
-                max=current;
-                year=i;
-            }
-        }
-        return year +1950;
     }
 }
