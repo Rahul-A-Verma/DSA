33c2a5eb35da0dfc5b9cab910c0edd9be8ed9336
Rahul Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2026-05-25 11:58:54 +0530

    Time: 4 ms (31.40%) | Memory: 47.2 MB (34.19%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+    
+           List<Integer> minarr = new ArrayList<>();
+           List<Integer> maxarr = new ArrayList<>();
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for(int i=0;i<matrix.length;i++){
+               int min=Integer.MAX_VALUE;
+            for(int j=0;j<matrix[0].length;j++){
+               if(matrix[i][j]<min){
+                min=matrix[i][j];
+               }
+            }
+            minarr.add(min);
+        }
+         for(int i=0;i<matrix[0].length;i++){
+               int max=0;
+            for(int j=0;j<matrix.length;j++){
+               if(matrix[j][i]>max){
+                max=matrix[j][i];
+               }
+            }
+            maxarr.add(max);
+        }
+
+        Set<Integer> set = new HashSet<>(minarr);
+        List<Integer> commonElements = new ArrayList<>();
+        for (int num : maxarr) {
+    if (set.contains(num)) {
+        commonElements.add(num);
+    }
+}
+        return commonElements;
+
+    }
+}
