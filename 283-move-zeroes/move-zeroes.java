dd184f312063dbb7031f2e9c0429deeb5c239a7c
Rahul Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2026-06-21 06:32:52 +0530

    Time: 3 ms (18.81%) | Memory: 47.7 MB (75.12%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,7 +3,6 @@ class Solution {
         int left =0;
         int right =0;
         for(int i =0;i<nums.length;i++){
-
             if(nums[right]!=0){
                 int temp = nums[left];
                 nums[left] =nums[right];
