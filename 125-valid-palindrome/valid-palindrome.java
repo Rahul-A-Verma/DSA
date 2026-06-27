cb6f8794967c86be0a2e147ed27597c58669a4a6
Rahul Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2026-06-27 10:50:45 +0530

    Time: 2 ms (99.23%) | Memory: 44.4 MB (57.44%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,27 +1,44 @@
 class Solution {
-    public boolean isAlphaNum(char c){
-        return (c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9');
-    }
     public boolean isPalindrome(String s) {
         int l=0;
-        int r =s.length()-1;
+        int r=s.length()-1;
+
         while(l<r){
-            if(!isAlphaNum(s.charAt(l))){
+            // if(!isChar(s.charAt(l))){
+            if(!Character.isLetterOrDigit(s.charAt(l))){
                 l++;
             }
-            else if(!isAlphaNum(s.charAt(r))){
+            // else if(!isChar(s.charAt(r))){
+            else if(!Character.isLetterOrDigit(s.charAt(r))){
                 r--;
             }
             else{
-                 if(Character.toLowerCase(s.charAt(l))==Character.toLowerCase(s.charAt(r))){
-                l++;
-                r--;
-            }
-                 else{
-                     return false;
-                 }
+                // if(isCase(s.charAt(l))==isCase(s.charAt(r))){
+                if(Character.toLowerCase(s.charAt(l))==Character.toLowerCase(s.charAt(r))){
+                    l++;
+                    r--;
+                }
+                else{
+                    return false;
+                }
             }
         }
         return true;
     }
+
+    // public static boolean isChar(char ch){
+    //     if((ch>='A'&& ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9')) {
+    //         return true;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // }
+    //  public static char isCase(char ch){
+    //     if (ch >= 'A' && ch <= 'Z') {
+    //     return (char) (ch + 32); 
+    // }
+    // return ch;
+       
+    //  }
 }
