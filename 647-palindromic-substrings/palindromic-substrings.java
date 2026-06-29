e7961eab3fefc6054f91a7eefee67ef6e41c1bcd
Rahul Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2026-06-29 19:17:44 +0530

    Time: 6 ms (60.45%) | Memory: 43 MB (42.52%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,24 +1,36 @@
 class Solution {
-    public int palin(String s, int l, int r){
-        int count =0;
-        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
-            count++;
+    public int countSubstrings(String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+
+            int odd = palin(s,i,i);
+            int even = palin(s,i,i+1);
+            count += odd+even;
+            // int l=i;
+            // int r=i;
+            // while(l<=r && l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            //     count++;
+            //     l--;
+            //     r++;
+            // }
+            // int l2=i;
+            // int r2=i+1;
+            // while(l2<=r2 && l2>=0 && r2<s.length() && s.charAt(l2)==s.charAt(r2)){
+            //     count++;
+            //     l2--;
+            //     r2++;
+            // }
+            
+        }      
+        return count;
+    }
+    public static int palin(String s, int l, int r){
+        int count=0;
+           while(l<=r && l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+                count++;
                 l--;
                 r++;
             }
             return count;
-        }
-    public int countSubstrings(String s) {
-        int count = 0;
-       
-        for(int i=0;i<s.length();i++){
-            int odd =palin(s, i ,i);
-            int even=palin(s,i,i+1);
-        
-            count +=odd+even;
-
-            
-        }
-        return count;
     }
 }
