b0efc1360c9e1571a550e5183981d7a28698dc4d
Rahul Verma <107047674+Rahul-A-Verma@users.noreply.github.com>
2026-07-31 13:39:00 +0530

    Time: 2 ms (95.56%) | Memory: 48.5 MB (24.33%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,37 +1,17 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        // for(int i=0;i<numbers.length-1;i++){
-        //     for(int j=i+1;j<numbers.length;j++){
-        //         if(numbers[i]+numbers[j]==target){
-        //             return new int[]{i + 1, j + 1};
-        //         }
-        //     }
-        // }
-        // return new int[]{-1,-1};
+        int p1=0;
+        int p2=numbers.length-1;
 
-        // HashMap <Integer, Integer> map = new HashMap<>();
-        // for(int i=0;i<numbers.length;i++){
-          
-        //     int key = target-numbers[i];
-        //     if(map.containsKey(key)){
-        //         return new int[] { map.get(key), i + 1 };
-        //     }
-        //       map.put(numbers[i],i+1);
-        // }
-        //   return new int[] {};
-
-        int l=0;
-        int r=numbers.length-1;
-        while(l<r){
-           
-            if(numbers[l]+numbers[r]==target){
-                return new int[]{l + 1, r + 1};
+        while(p1<p2){
+            if(numbers[p1]+numbers[p2]==target){
+                return new int[]{p1+1,p2+1};
             }
-            else if(numbers[l]+numbers[r]>target){
-                r--;
+            else if(numbers[p1]+numbers[p2]>target){
+                p2--;
             }
             else{
-                l++;
+                p1++;
             }
         }
         return new int[]{};
